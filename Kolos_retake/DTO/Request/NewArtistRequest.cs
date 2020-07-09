9134db3f7d17deb7c5a8535e0cb980e0af7532b3
Kacper Bernat-Januszkiewicz <kbernatjanuszkiewicz@finitec.pl>
2020-07-09 18:58:03 +0200

    done

---
+++ Kolos_retake/DTO/Request/NewArtistRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kolos_retake.DTO.Request
+{
+    public class NewArtistRequest
+    {
+    }
+}
