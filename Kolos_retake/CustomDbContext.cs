b3d8209ad34d683a3ad3ae72f95d30d749b89fe6
Kacper Bernat-Januszkiewicz <kbernatjanuszkiewicz@finitec.pl>
2020-07-09 16:41:25 +0200

    init

---
+++ Kolos_retake/CustomDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kolos_retake
+{
+    public class CustomDbContext : DbContext
+    {
+
+        public CustomDbContext()
+        {
+
+        }
+
+        public CustomDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Kolos_retake/Services/SqlServerCustomDbService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kolos_retake.Services
+{
+    public class SqlServerCustomDbService : ICustomDbService
+    {
+        private readonly CustomDbContext _dbContext;
+
+        public SqlServerCustomDbService(CustomDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+    }
+}
